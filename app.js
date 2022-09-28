4a98d54169c25a6c74b6f505fc11c3e24f9bedce
SadiKISLAR <sadi22kislar@gmail.com>
2022-09-28 11:05:58 +0300

    operators

--- app.js
+++ app.js
@@ -5,4 +5,70 @@ const btnContainer = document.querySelector(".button-container");
 let num1;
 let num2;
 let operator;
+let num1Done = false;
+let floating = false;
 
+btnContainer.addEventListener("click", (e)=> {
+    if(e.target.id == "n0"){
+        input.innerText+= 0;
+    }
+    else if(e.target.id == "n1"){
+        input.innerText+= 1;
+    }
+    else if(e.target.id == "n2"){
+        input.innerText+= 2;
+    }
+    else if(e.target.id == "n3"){
+        input.innerText+= 3;
+    }
+    else if(e.target.id == "n4"){
+        input.innerText+= 4;
+    }
+    else if(e.target.id == "n5"){
+        input.innerText+= 5;
+    }
+    else if(e.target.id == "n6"){
+        input.innerText+= 6;
+    }
+    else if(e.target.id == "n7"){
+        input.innerText+= 7;
+    }
+    else if(e.target.id == "n8"){
+        input.innerText+= 8;
+    }
+    else if(e.target.id == "n9"){
+        input.innerText+= 9;
+    }
+    else if(e.target.id == "division" && !num1Done && input.innerText) {
+        input2.innerText += input.innerText + "÷";
+        num1 = Number(input.innerText).toFixed(4);
+        operator = "division";
+        input.innerText = "";
+    }
+    else if(e.target.id == "multi" && !num1Done && input.innerText) {
+        input2.innerText += input.innerText + "*"
+        num1 = Number(input.innerText).toFixed(4);
+        operator = "multi";
+        input.innerText = "";
+    }
+    else if(e.target.id == "sub" && !num1Done && input.innerText) {
+        input2.innerText += input.innerText + "-";
+        num1 = Number(input.innerText).toFixed(4);
+        operator = "sub";
+        input.innerText = "";
+    } 
+    else if(e.target.id == "add" && !num1Done && input.innerText) {
+        input2.innerText += input.innerText + "+";
+        num1 = Number(input.innerText).toFixed(4);
+        operator = "add";
+        input.innerText = "";
+    }
+    else if(e.target.id == "equal" && !num1Done && input.innerText) {
+        input2.innerText += input.innerText + "=";
+        num1 = Number(input.innerText).toFixed(4);
+        operator = "equal";
+        input.innerText = "";
+    }
+
+
+});
