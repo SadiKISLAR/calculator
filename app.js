ca9b6832b6a78c1a3b68c8e66d88d055d70352a0
SadiKISLAR <sadi22kislar@gmail.com>
2022-09-28 09:41:30 +0300

    variables

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input");
+const input2 = document.querySelector(".input2");
+const btnContainer = document.querySelector(".button-container");
+
+let num1;
+let num2;
+let operator;
+
